aa233abb4187ebb045566b9b24bd44b7cac89399
Bat <baptiste@gelez.xyz>
2018-05-18 23:28:40 +0100

    Remove useless trait

--- src/activity_pub/object.rs
+++ src/activity_pub/object.rs
@@ -1,14 +1,9 @@
 use diesel::PgConnection;
 use serde_json;
 
-use activity_pub::actor::Actor;
-
 pub trait Object {
     fn serialize(&self, conn: &PgConnection) -> serde_json::Value;
 
     fn compute_id(&self, conn: &PgConnection) -> String;
 }
 
-pub trait Attribuable {
-    fn set_attribution<T>(&self, by: &T) where T: Actor;
-}
