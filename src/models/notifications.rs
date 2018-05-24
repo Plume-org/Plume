a0b4a6eacb30891f0df865fff82471c67e58e056
Bat <baptiste@gelez.xyz>
2018-05-24 11:12:27 +0100

    Order notifications by creation date

--- src/models/notifications.rs
+++ src/models/notifications.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDateTime;
 use diesel::{self, PgConnection, RunQueryDsl, QueryDsl, ExpressionMethods};
 
 use models::users::User;
@@ -9,7 +10,8 @@ pub struct Notification {
     pub title: String,
     pub content: Option<String>,
     pub link: Option<String>,
-    pub user_id: i32
+    pub user_id: i32,
+    pub creation_date: NaiveDateTime
 }
 
 #[derive(Insertable)]
@@ -39,6 +41,7 @@ impl Notification {
 
     pub fn find_for_user(conn: &PgConnection, user: &User) -> Vec<Notification> {
         notifications::table.filter(notifications::user_id.eq(user.id))
+            .order_by(notifications::creation_date.desc())
             .load::<Notification>(conn)
             .expect("Couldn't load user notifications")
     }
