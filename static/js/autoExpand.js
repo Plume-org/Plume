4d4a58361b009af4544d8072099a32aa8ef2d276
Bat <baptiste@gelez.xyz>
2018-09-01 21:42:53 +0100

    Use Standard JS for JS code

--- static/js/autoExpand.js
+++ static/js/autoExpand.js
@@ -1,14 +1,9 @@
-function autosize(){
-  const el = this;
-  el.style.height = 'auto';
-  el.style.height = (el.scrollHeight ) + 'px';
+function autosize () {
+  const el = this
+  el.style.height = 'auto'
+  el.style.height = `${el.scrollHeight}px`
 }
 
-const articleContent = document.querySelector('#content');
-let offset = 0;
-let style = window.getComputedStyle(articleContent, null);
-
-offset += parseInt(style['paddingTop']) + parseInt(style['paddingBottom']);
-autosize.bind(articleContent)();
-articleContent.addEventListener('keyup', autosize);
-
+const articleContent = document.querySelector('#content')
+autosize.bind(articleContent)()
+articleContent.addEventListener('keyup', autosize)
