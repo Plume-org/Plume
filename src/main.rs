Plume-org/Plume

commit fceb9ab0cd9ac33f36cb2623776ddfd6808c73d8

Update cookie management a bit
Update to latest rocket_csrf
Make user_id a samesite lax cookie (see https://github.com/Plume-org/Plume/issues/233#issuecomment-422660275)

Cargo.toml
@@ -56,7 +56,7 @@ rev = "55459db7732b9a240826a5c120c650f87e3372ce"
 
 [dependencies.rocket_csrf]
 git = "https://github.com/fdb-hiroshima/rocket_csrf"
-rev = "5d23ba4c6c2ee4c41040d428d24344db3d29997f"
+rev = "b326a9893a1849c9abdb39cab9fd7c4a52eb9674"
 
 [dependencies.rocket_i18n]
 git = "https://github.com/BaptisteGelez/rocket_i18n"

src/main.rs
@@ -165,7 +165,7 @@ fn main() {
                 .add_exceptions(vec![
                     ("/inbox".to_owned(), "/inbox".to_owned(), rocket::http::Method::Post),
                     ("/@/<name>/inbox".to_owned(), "/@/<name>/inbox".to_owned(), rocket::http::Method::Post),
-                    ("/~/<blog>/<slug>".to_owned(), "/~/<blog>/<slug>".to_owned(), rocket::http::Method::Post),
+                    ("/login".to_owned(), "/login".to_owned(), rocket::http::Method::Post),
                 ])
                 .finalize().unwrap())
         .launch();

src/routes/session.rs
@@ -1,5 +1,5 @@
 use rocket::{
-    http::{Cookie, Cookies, uri::Uri},
+    http::{Cookie, Cookies, SameSite, uri::Uri},
     response::Redirect,
     request::{LenientForm,FlashMessage}
 };
@@ -72,7 +72,9 @@ fn create(conn: DbConn, data: LenientForm<LoginForm>, flash: Option<FlashMessage
     }
 
     if errors.is_empty() {
-        cookies.add_private(Cookie::new(AUTH_COOKIE, user.unwrap().id.to_string()));
+        cookies.add_private(Cookie::build(AUTH_COOKIE, user.unwrap().id.to_string())
+                                            .same_site(SameSite::Lax)
+                                            .finish());
 
         let destination = flash
             .and_then(|f| if f.name() == "callback" {

commit d3ed2d8af5b7dd5ee4a682701d3c64c25a000d18

Fix regression and update rocket_csrf
Fix account creation, introduced by fceb9ab
Update to latest rocket_csrf

Cargo.toml
@@ -56,7 +56,7 @@ rev = "55459db7732b9a240826a5c120c650f87e3372ce"
 
 [dependencies.rocket_csrf]
 git = "https://github.com/fdb-hiroshima/rocket_csrf"
-rev = "b326a9893a1849c9abdb39cab9fd7c4a52eb9674"
+rev = "2805ce5dbae4a6441208484426440885a5640a6a"
 
 [dependencies.rocket_i18n]
 git = "https://github.com/BaptisteGelez/rocket_i18n"

src/main.rs
@@ -166,6 +166,7 @@ fn main() {
                     ("/inbox".to_owned(), "/inbox".to_owned(), rocket::http::Method::Post),
                     ("/@/<name>/inbox".to_owned(), "/@/<name>/inbox".to_owned(), rocket::http::Method::Post),
                     ("/login".to_owned(), "/login".to_owned(), rocket::http::Method::Post),
+                    ("/users/new".to_owned(), "/users/new".to_owned(), rocket::http::Method::Post),
                 ])
                 .finalize().unwrap())
         .launch();