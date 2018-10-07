e46857bdf31f8b85b553bbab52fcbe0ab79a6fe0
Bat <baptiste@gelez.xyz>
2018-10-07 16:30:48 +0100

    Add a create method to the post REST API

--- plume-api/src/posts.rs
+++ plume-api/src/posts.rs
@@ -1,11 +1,18 @@
 use canapi::Endpoint;
 
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Clone, Default, Serialize, Deserialize)]
 pub struct PostEndpoint {
     pub id: Option<i32>,
     pub title: Option<String>,
     pub subtitle: Option<String>,
-    pub content: Option<String>
+    pub content: Option<String>,
+    pub source: Option<String>,
+    pub author: Option<String>,
+    pub blog_id: Option<i32>,
+    pub published: Option<bool>,
+    pub creation_date: Option<String>,
+    pub license: Option<String>,
+    pub tags: Option<Vec<String>>,
 }
 
 api!("/api/v1/posts" => PostEndpoint);
--- src/api/posts.rs
+++ src/api/posts.rs
@@ -1,6 +1,7 @@
 use canapi::Provider;
 use rocket::http::uri::Origin;
 use rocket_contrib::json::Json;
+use scheduled_thread_pool::ScheduledThreadPool;
 use serde_json;
 use serde_qs;
 
@@ -12,17 +13,28 @@ use plume_models::{
     search::Searcher as UnmanagedSearcher,
 };
 use api::authorization::*;
-use Searcher;
+use {Searcher, Worker};
 
 #[get("/posts/<id>")]
-pub fn get(id: i32, conn: DbConn, auth: Option<Authorization<Read, Post>>, search: Searcher) -> Json<serde_json::Value> {
-    let post = <Post as Provider<(&Connection, &UnmanagedSearcher, Option<i32>)>>::get(&(&*conn, &search, auth.map(|a| a.0.user_id)), id).ok();
+pub fn get(id: i32, conn: DbConn, worker: Worker, auth: Option<Authorization<Read, Post>>, search: Searcher) -> Json<serde_json::Value> {
+    let post = <Post as Provider<(&Connection, &ScheduledThreadPool, &UnmanagedSearcher, Option<i32>)>>
+        ::get(&(&*conn, &worker, &search, auth.map(|a| a.0.user_id)), id).ok();
     Json(json!(post))
 }
 
 #[get("/posts")]
-pub fn list(conn: DbConn, uri: &Origin, auth: Option<Authorization<Read, Post>>, search: Searcher) -> Json<serde_json::Value> {
+pub fn list(conn: DbConn, uri: &Origin, worker: Worker, auth: Option<Authorization<Read, Post>>, search: Searcher) -> Json<serde_json::Value> {
     let query: PostEndpoint = serde_qs::from_str(uri.query().unwrap_or("")).expect("api::list: invalid query error");
-    let post = <Post as Provider<(&Connection, &UnmanagedSearcher, Option<i32>)>>::list(&(&*conn, &search, auth.map(|a| a.0.user_id)), query);
+    let post = <Post as Provider<(&Connection, &ScheduledThreadPool, &UnmanagedSearcher, Option<i32>)>>
+        ::list(&(&*conn, &worker, &search, auth.map(|a| a.0.user_id)), query);
     Json(json!(post))
+}
+
+#[post("/posts", data = "<payload>")]
+pub fn create(conn: DbConn, payload: Json<PostEndpoint>, worker: Worker, auth: Authorization<Write, Post>, search: Searcher) -> Json<serde_json::Value> {
+    let new_post = <Post as Provider<(&Connection, &ScheduledThreadPool, &UnmanagedSearcher, Option<i32>)>>
+        ::create(&(&*conn, &worker, &search, Some(auth.0.user_id)), (*payload).clone());
+    Json(new_post.map(|p| json!(p)).unwrap_or(json!({
+        "error": "Invalid data, couldn't create new post"
+    })))
 }
