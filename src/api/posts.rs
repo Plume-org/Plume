31641b1ea1d28973c276baebad7ec2f959c570dc
Baptiste Gelez <baptiste@gelez.xyz>
2018-10-23 10:37:24 +0100

    New request guard: Authorization<Action, Scope>
    Filter requests that don't have an API token authorized to read or write
    a specific scope;

--- src/api/posts.rs
+++ src/api/posts.rs
@@ -7,31 +7,20 @@ use serde_qs;
 use plume_api::posts::PostEndpoint;
 use plume_models::{
     Connection,
-    api_tokens::ApiToken,
     db_conn::DbConn,
     posts::Post,
 };
+use api::authorization::*;
 
 #[get("/posts/<id>")]
-fn get(id: i32, conn: DbConn, token: ApiToken) -> Json<serde_json::Value> {
-    if token.can_read("posts") {
-        let post = <Post as Provider<Connection>>::get(&*conn, id).ok();
-        Json(json!(post))
-    } else {
-        Json(json!({
-            "error": "Unauthorized"
-        }))
-    }
+fn get(id: i32, conn: DbConn, _auth: Authorization<Read, Post>) -> Json<serde_json::Value> {
+    let post = <Post as Provider<Connection>>::get(&*conn, id).ok();
+    Json(json!(post))
 }
 
 #[get("/posts")]
-fn list(conn: DbConn, uri: &Origin, token: ApiToken) -> Json<serde_json::Value> {
-    if token.can_read("posts") {
-        let query: PostEndpoint = serde_qs::from_str(uri.query().unwrap_or("")).expect("api::list: invalid query error");
-        let post = <Post as Provider<Connection>>::list(&*conn, query);
-        Json(json!(post))
-    } else {
-        Json(json!({
-            "error": "Unauthorized"
-        }))
-    }}
+fn list(conn: DbConn, uri: &Origin, _auth: Authorization<Read, Post>) -> Json<serde_json::Value> {
+    let query: PostEndpoint = serde_qs::from_str(uri.query().unwrap_or("")).expect("api::list: invalid query error");
+    let post = <Post as Provider<Connection>>::list(&*conn, query);
+    Json(json!(post))
+}
