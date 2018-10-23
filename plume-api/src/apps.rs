0dbf9f519e638e2354c8388d6b646a5f1d09bd08
Baptiste Gelez <baptiste@gelez.xyz>
2018-10-23 10:43:14 +0100

    AppEndpoint.name is required for both the client and server

--- plume-api/src/apps.rs
+++ plume-api/src/apps.rs
@@ -3,7 +3,7 @@ use canapi::Endpoint;
 #[derive(Clone, Default, Serialize, Deserialize)]
 pub struct AppEndpoint {
     pub id: Option<i32>,
-    pub name: Option<String>,
+    pub name: String,
     pub website: Option<String>,
     pub redirect_uri: Option<String>,
     pub client_id: Option<String>,
