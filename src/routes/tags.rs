93b2568fa9e5315eb8931cd83d4d7cf5ff7e4e82
Baptiste Gelez <baptiste@gelez.xyz>
2018-11-28 19:44:24 +0100

    Ructe: tag page

--- src/routes/tags.rs
+++ src/routes/tags.rs
@@ -1,26 +1,25 @@
-use rocket_contrib::templates::Template;
-use serde_json;
+use rocket_i18n::I18n;
 
 use plume_models::{
     db_conn::DbConn,
     posts::Post,
     users::User,
 };
-use routes::Page;
+use routes::{Ructe, Page};
 
 #[get("/tag/<name>")]
-pub fn tag(user: Option<User>, conn: DbConn, name: String) -> Template {
-    paginated_tag(user, conn, name, Page::first())
+pub fn tag(user: Option<User>, conn: DbConn, name: String, intl: I18n) -> Ructe {
+    paginated_tag(user, conn, name, Page::first(), intl)
 }
 
 #[get("/tag/<name>?<page>")]
-pub fn paginated_tag(user: Option<User>, conn: DbConn, name: String, page: Page) -> Template {
+pub fn paginated_tag(user: Option<User>, conn: DbConn, name: String, page: Page, intl: I18n) -> Ructe {
     let posts = Post::list_by_tag(&*conn, name.clone(), page.limits());
-    Template::render("tags/index", json!({
-        "tag": name.clone(),
-        "account": user.map(|u| u.to_json(&*conn)),
-        "articles": posts.into_iter().map(|p| p.to_json(&*conn)).collect::<Vec<serde_json::Value>>(),
-        "page": page.0,
-        "n_pages": Page::total(Post::count_for_tag(&*conn, name) as i32)
-    }))
+    render!(tags::index(
+        &(&*conn, &intl.catalog, user),
+        name.clone(),
+        posts,
+        page.0,
+        Page::total(Post::count_for_tag(&*conn, name) as i32)
+    ))
 }
