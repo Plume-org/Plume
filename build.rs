e25081c8a7fc8600027b609a719003b615b5aa33
Baptiste Gelez <baptiste@gelez.xyz>
2018-12-04 14:23:58 +0100

    Compile japanese translation

--- build.rs
+++ build.rs
@@ -10,6 +10,6 @@ fn main() {
     compile_templates(&in_dir, &out_dir).expect("compile templates");
 
     println!("cargo:rerun-if-changed=po");
-    rocket_i18n::update_po("plume", &["de", "en", "fr", "gl", "it", "nb", "pl", "ru"]);
-    rocket_i18n::compile_po("plume", &["de", "en", "fr", "gl", "it", "nb", "pl", "ru"]);
+    rocket_i18n::update_po("plume", &["de", "en", "fr", "gl", "it", "ja", "nb", "pl", "ru"]);
+    rocket_i18n::compile_po("plume", &["de", "en", "fr", "gl", "it", "ja", "nb", "pl", "ru"]);
 }
