eda0b3099b95b2442689c5659a88ba8867c93b4d
Baptiste Gelez <baptiste@gelez.xyz>
2018-12-15 16:32:00 +0100

    Replace CSS with SCSS

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 extern crate ructe;
 extern crate rocket_i18n;
+extern crate rsass;
 use ructe::*;
-use std::{env, path::PathBuf};
+use std::{env, fs::File, io::Write, path::PathBuf};
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -12,4 +13,12 @@ fn main() {
     println!("cargo:rerun-if-changed=po");
     rocket_i18n::update_po("plume", &["de", "en", "fr", "gl", "it", "ja", "nb", "pl", "ru"]);
     rocket_i18n::compile_po("plume", &["de", "en", "fr", "gl", "it", "ja", "nb", "pl", "ru"]);
+
+    println!("cargo:rerun-if-changed=static/css");
+    let mut out = File::create("static/css/main.css").expect("Couldn't create main.css");
+    println!("annana");
+    out.write_all(
+        &rsass::compile_scss_file("static/css/main.scss".as_ref(), rsass::OutputStyle::Compressed)
+            .expect("Error during SCSS compilation")
+    ).expect("Couldn't write CSS output");
 }
