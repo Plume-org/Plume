f2c699764aa6ffacf1e1191591fbd62b71328ca8
mcrosson <mcrosson_github@nusku.net>
2018-12-31 11:28:49 -0500

    Remove saving of path as it's not actually not necessary

--- script/plume-front.sh
+++ script/plume-front.sh
@@ -3,11 +3,9 @@
 ARCH=`arch`
 
 if [ "$ARCH" == "aarch64" -o "$ARCH" == "armv7l" ] ; then
-    PATH_OLD=${PATH}
     export PATH=/opt/local/llvm/bin:${PATH}
     cd /app
     RUSTFLAGS="-C linker=lld" cargo web deploy -p plume-front
-    export PATH=${PATH_OLD}
 else
     cargo web deploy -p plume-front
 fi
