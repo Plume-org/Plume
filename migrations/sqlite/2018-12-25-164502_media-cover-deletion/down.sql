741f4aade20d3530d31b88d506b37fabc3c62b1f
Trinity Pointard <trinity.pointard@insa-rennes.fr>
2019-01-05 19:35:25 +0100

    Fix sqlite migrations

--- migrations/sqlite/2018-12-25-164502_media-cover-deletion/down.sql
+++ migrations/sqlite/2018-12-25-164502_media-cover-deletion/down.sql
@@ -1,4 +1,21 @@
 -- This file should undo anything in `up.sql`
 
-ALTER TABLE posts DROP CONSTRAINT posts_cover_id_fkey;
-ALTER TABLE posts ADD CONSTRAINT posts_cover_id_fkey FOREIGN KEY (cover_id) REFERENCES medias(id);
+CREATE TABLE posts2 (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    blog_id INTEGER REFERENCES blogs(id) ON DELETE CASCADE NOT NULL,
+    slug VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    content TEXT NOT NULL DEFAULT '',
+    published BOOLEAN NOT NULL DEFAULT 'f',
+    license VARCHAR NOT NULL DEFAULT 'CC-BY-SA',
+    creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ap_url VARCHAR NOT NULL DEFAULT '' UNIQUE,
+    subtitle TEXT NOT NULL DEFAULT '',
+    source TEXT NOT NULL DEFAULT '',
+    cover_id INTEGER REFERENCES medias(id) DEFAULT NULL,
+    CONSTRAINT blog_authors_unique UNIQUE (blog_id, slug)
+);
+
+INSERT INTO posts2 SELECT * from posts;
+DROP TABLE posts;
+ALTER TABLE posts2 RENAME TO posts;
--- migrations/sqlite/2018-12-25-164502_media-cover-deletion/up.sql
+++ migrations/sqlite/2018-12-25-164502_media-cover-deletion/up.sql
@@ -1,4 +1,21 @@
 -- Your SQL goes here
 
-ALTER TABLE posts DROP CONSTRAINT posts_cover_id_fkey;
-ALTER TABLE posts ADD CONSTRAINT posts_cover_id_fkey FOREIGN KEY (cover_id) REFERENCES medias(id) ON DELETE SET NULL;
+CREATE TABLE posts2 (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    blog_id INTEGER REFERENCES blogs(id) ON DELETE CASCADE NOT NULL,
+    slug VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    content TEXT NOT NULL DEFAULT '',
+    published BOOLEAN NOT NULL DEFAULT 'f',
+    license VARCHAR NOT NULL DEFAULT 'CC-BY-SA',
+    creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ap_url VARCHAR NOT NULL DEFAULT '' UNIQUE,
+    subtitle TEXT NOT NULL DEFAULT '',
+    source TEXT NOT NULL DEFAULT '',
+    cover_id INTEGER REFERENCES medias(id) ON DELETE SET NULL DEFAULT NULL,
+    CONSTRAINT blog_authors_unique UNIQUE (blog_id, slug)
+);
+
+INSERT INTO posts2 SELECT * from posts;
+DROP TABLE posts;
+ALTER TABLE posts2 RENAME TO posts;
