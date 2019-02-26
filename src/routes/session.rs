25d537feaebeb619a9045faeeb39aadaa2c076fc
Baptiste Gelez <baptiste@gelez.xyz>
2019-02-26 16:11:10 +0100

    Forgot a 0 è_é

--- src/routes/session.rs
+++ src/routes/session.rs
@@ -144,7 +144,7 @@ pub fn password_reset_request(
 ) -> Ructe {
     let mut requests = requests.lock().unwrap();
     // Remove outdated requests (more than 1 day old) to avoid the list to grow too much
-    requests.retain(|r| r.creation_date.elapsed().as_secs() < 24 * 60 * 6);
+    requests.retain(|r| r.creation_date.elapsed().as_secs() < 24 * 60 * 60);
 
     if User::find_by_email(&*conn, &form.email).is_ok() && !requests.iter().any(|x| x.mail == form.email.clone()) {
         let id = plume_common::utils::random_hex();
