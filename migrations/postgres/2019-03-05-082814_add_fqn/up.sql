d762d58eb7c8c3dcfe78e24f8f80e0b6287094de
Baptiste Gelez <baptiste@gelez.xyz>
2019-03-05 19:57:40 +0100

    The local instance may have an ID that is not 1

--- migrations/postgres/2019-03-05-082814_add_fqn/up.sql
+++ migrations/postgres/2019-03-05-082814_add_fqn/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 ALTER TABLE blogs ADD COLUMN fqn TEXT NOT NULL DEFAULT '';
 UPDATE blogs SET fqn =
-	(CASE WHEN instance_id = 1 THEN
+	(CASE WHEN (SELECT local FROM instances WHERE id = instance_id) THEN
 		actor_id
 	ELSE
 		(actor_id || '@' || (SELECT public_domain FROM instances WHERE id = instance_id LIMIT 1))
@@ -10,7 +10,7 @@ WHERE fqn = '';
 
 ALTER TABLE users ADD COLUMN fqn TEXT NOT NULL DEFAULT '';
 UPDATE users SET fqn =
-	(CASE WHEN instance_id = 1 THEN
+	(CASE WHEN (SELECT local FROM instances WHERE id = instance_id) THEN
 		username
 	ELSE
 		(username || '@' || (SELECT public_domain FROM instances WHERE id = instance_id LIMIT 1))
