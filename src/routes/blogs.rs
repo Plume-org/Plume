ac697ac9fafcd0a5c8bd3c5525818c9a94a8bf04
Marek Ľach <45913977+marek-lach@users.noreply.github.com>
2019-04-01 13:56:26 +0200

    Update blogs.rs

--- src/routes/blogs.rs
+++ src/routes/blogs.rs
@@ -68,7 +68,7 @@ pub fn new_auth(i18n: I18n) -> Flash<Redirect> {
     utils::requires_login(
         &i18n!(
             i18n.catalog,
-            "You need to be logged in order to create a new blog"
+            "To create a new blog, you need to be logged in"
         ),
         uri!(new),
     )
