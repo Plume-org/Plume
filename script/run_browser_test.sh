ab446fb723515c79392cc8366fb214e04b699dc8
Trinity Pointard <trinity.pointard@insa-rennes.fr>
2019-04-02 16:01:28 +0200

    run migrations before tests

--- script/run_browser_test.sh
+++ script/run_browser_test.sh
@@ -7,6 +7,8 @@ mkdir -p "target/cov/plume"
 mkdir -p "target/cov/plm"
 plm='kcov --exclude-pattern=/.cargo,/usr/lib --verify target/cov/plm plm'
 
+diesel migration run
+diesel migration redo
 $plm instance new -d plume-test.local -n plume-test
 $plm users new -n admin -N 'Admin' -e 'email@exemple.com' -p 'password'
 $plm search init
