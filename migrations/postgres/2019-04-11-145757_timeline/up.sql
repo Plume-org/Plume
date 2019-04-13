2d6c758c8fef5acdb065f66d052caffc617bfc3c
Trinity Pointard <trinity.pointard@insa-rennes.fr>
2019-04-13 18:58:22 +0200

    tests sql things

--- migrations/postgres/2019-04-11-145757_timeline/up.sql
+++ migrations/postgres/2019-04-11-145757_timeline/up.sql
@@ -12,3 +12,23 @@ CREATE TABLE timeline(
 	post_id integer NOT NULL REFERENCES posts ON DELETE CASCADE,
 	timeline_id integer NOT NULL REFERENCES timeline_definition ON DELETE CASCADE
 );
+
+CREATE TABLE lists(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	user_id integer REFERENCES users ON DELETE CASCADE,
+	type CHAR NOT NULL
+);
+
+CREATE TABLE list_elems(
+	id SERIAL PRIMARY KEY,
+	list_id integer NOT NULL REFERENCES lists ON DELETE CASCADE,
+	blog_id integer REFERENCES blogs ON DELETE CASCADE,
+	user_id integer REFERENCES users ON DELETE CASCADE,
+	word VARCHAR,
+	CONSTAINT list_elems_one_no_null CHECK(
+		CASE WHEN blog_id IS NOT NULL THEN 1 ELSE 0 END +
+		CASE WHEN user_id IS NOT NULL THEN 1 ELSE 0 END +
+		CASE WHEN word IS NOT NULL THEN 1 ELSE 0 END
+		= 1)
+);
