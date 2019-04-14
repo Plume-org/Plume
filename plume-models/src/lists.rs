9d8b4da5a642fbb1296dc3e58f099f3fbc3dd121
Trinity Pointard <trinity.pointard@insa-rennes.fr>
2019-04-14 15:08:08 +0200

    end adding lists
    still miss tests and query integration

--- plume-models/src/lists.rs
+++ plume-models/src/lists.rs
@@ -1,10 +1,12 @@
 use diesel::{self, ExpressionMethods, QueryDsl, RunQueryDsl};
 
-use schema::{list_elems, lists};
+use blogs::Blog;
+use schema::{list_elems, lists, users, blogs};
 use std::convert::{TryFrom, TryInto};
 use users::User;
 use {Connection, Error, Result};
 
+/// Represent what a list is supposed to store. Represented in database as an integer
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum ListType {
     User,
@@ -65,11 +67,11 @@ struct ListElem {
 
 #[derive(Default, Insertable)]
 #[table_name = "list_elems"]
-struct NewListElem {
+struct NewListElem<'a> {
     pub list_id: i32,
     pub user_id: Option<i32>,
     pub blog_id: Option<i32>,
-    pub word: Option<String>,
+    pub word: Option<&'a str>,
 }
 
 impl List {
@@ -100,62 +102,213 @@ impl List {
         )
     }
 
+    /// Returns the kind of a list
     pub fn kind(&self) -> ListType {
         self.type_
             .try_into()
             .expect("invalide list was constructed")
     }
 
+    /// Return Ok(true) if the list contain the given user, Ok(false) otherwiser,
+    /// and Err(_) on error
     pub fn contains_user(&self, conn: &Connection, user: i32) -> Result<bool> {
         private::ListElem::user_in_list(conn, self, user)
     }
 
+    /// Return Ok(true) if the list contain the given blog, Ok(false) otherwiser,
+    /// and Err(_) on error
     pub fn contains_blog(&self, conn: &Connection, blog: i32) -> Result<bool> {
         private::ListElem::user_in_list(conn, self, blog)
     }
 
+    /// Return Ok(true) if the list contain the given word, Ok(false) otherwiser,
+    /// and Err(_) on error
     pub fn contains_word(&self, conn: &Connection, word: &str) -> Result<bool> {
         private::ListElem::word_in_list(conn, self, word)
     }
 
+    /// Return Ok(true) if the list match the given prefix, Ok(false) otherwiser,
+    /// and Err(_) on error
     pub fn contains_prefix(&self, conn: &Connection, word: &str) -> Result<bool> {
         private::ListElem::prefix_in_list(conn, self, word)
     }
 
+    /// Insert new users in a list
     /// returns Ok(false) if this list isn't for users
-    pub fn add_users(&self, conn: &Connection, user: &[i32]) -> Result<bool> {
+    pub fn add_users(&self, conn: &Connection, users: &[i32]) -> Result<bool> {
         if self.kind() != ListType::User {
             return Ok(false);
         }
-        let _ = (conn, user);
-        unimplemented!();
+        diesel::insert_into(list_elems::table)
+            .values(users.iter()
+                    .map(|u| NewListElem {
+                        list_id: self.id, 
+                        user_id: Some(*u),
+                        blog_id: None,
+                        word: None,
+                    })
+                    .collect::<Vec<_>>()
+                    )
+            .execute(conn)?;
+        Ok(true)
     }
 
+    /// Insert new blogs in a list
     /// returns Ok(false) if this list isn't for blog
-    pub fn add_blogs(&self, conn: &Connection, blog: &[i32]) -> Result<bool> {
+    pub fn add_blogs(&self, conn: &Connection, blogs: &[i32]) -> Result<bool> {
         if self.kind() != ListType::Blog {
             return Ok(false);
         }
-        let _ = (conn, blog);
-        unimplemented!();
+        diesel::insert_into(list_elems::table)
+            .values(blogs.iter()
+                    .map(|b| NewListElem {
+                        list_id: self.id, 
+                        user_id: None,
+                        blog_id: Some(*b),
+                        word: None,
+                    })
+                    .collect::<Vec<_>>()
+                    )
+            .execute(conn)?;
+        Ok(true)
     }
 
+    /// Insert new words in a list
     /// returns Ok(false) if this list isn't for words
-    pub fn add_words(&self, conn: &Connection, word: &[&str]) -> Result<bool> {
+    pub fn add_words(&self, conn: &Connection, words: &[&str]) -> Result<bool> {
         if self.kind() != ListType::Word {
             return Ok(false);
         }
-        let _ = (conn, word);
-        unimplemented!();
+        diesel::insert_into(list_elems::table)
+            .values(words.iter()
+                    .map(|w| NewListElem {
+                        list_id: self.id, 
+                        user_id: None,
+                        blog_id: None,
+                        word: Some(w),
+                    })
+                    .collect::<Vec<_>>()
+                    )
+            .execute(conn)?;
+        Ok(true)
     }
 
+    /// Insert new prefixes in a list
     /// returns Ok(false) if this list isn't for prefix
-    pub fn add_prefixs(&self, conn: &Connection, prefix: &[&str]) -> Result<bool> {
+    pub fn add_prefixes(&self, conn: &Connection, prefixes: &[&str]) -> Result<bool> {
         if self.kind() != ListType::Prefix {
             return Ok(false);
         }
-        let _ = (conn, prefix);
-        unimplemented!();
+        diesel::insert_into(list_elems::table)
+            .values(prefixes.iter()
+                    .map(|p| NewListElem {
+                        list_id: self.id, 
+                        user_id: None,
+                        blog_id: None,
+                        word: Some(p),
+                    })
+                    .collect::<Vec<_>>()
+                    )
+            .execute(conn)?;
+        Ok(true)
+    }
+
+    /// Get all users in the list
+    pub fn list_users(&self, conn: &Connection) -> Result<Vec<User>> {
+        list_elems::table
+            .filter(list_elems::list_id.eq(self.id))
+            .inner_join(users::table)
+            .select(users::all_columns)
+            .load(conn)
+            .map_err(Error::from)
+    }
+
+    /// Get all blogs in the list
+    pub fn list_blogs(&self, conn: &Connection) -> Result<Vec<Blog>> {
+        list_elems::table
+            .filter(list_elems::list_id.eq(self.id))
+            .inner_join(blogs::table)
+            .select(blogs::all_columns)
+            .load(conn)
+            .map_err(Error::from)
+    }
+
+    /// Get all words in the list
+    pub fn list_words(&self, conn: &Connection) -> Result<Vec<String>> {
+        if self.kind() != ListType::Word {
+            return Ok(vec![]);
+        }
+        list_elems::table
+            .filter(list_elems::list_id.eq(self.id))
+            .filter(list_elems::word.is_not_null())
+            .select(list_elems::word)
+            .load::<Option<String>>(conn)
+            .map_err(Error::from)
+            .map(|r| {
+                r.into_iter()
+                    .filter_map(|o| o)
+                    .collect::<Vec<String>>()
+            })
+    }
+
+    /// Get all prefixes in the list
+    pub fn list_prefixes(&self, conn: &Connection) -> Result<Vec<String>> {
+        if self.kind() != ListType::Prefix {
+            return Ok(vec![]);
+        }
+        list_elems::table
+            .filter(list_elems::list_id.eq(self.id))
+            .filter(list_elems::word.is_not_null())
+            .select(list_elems::word)
+            .load::<Option<String>>(conn)
+            .map_err(Error::from)
+            .map(|r| {
+                r.into_iter()
+                    .filter_map(|o| o)
+                    .collect::<Vec<String>>()
+            })
+    }
+
+    pub fn clear(&self, conn: &Connection) -> Result<()> {
+        diesel::delete(
+                list_elems::table
+                    .filter(list_elems::list_id.eq(self.id))
+            )
+            .execute(conn)
+            .map(|_| ())
+            .map_err(Error::from)
+    }
+
+    pub fn set_users(&self, conn: &Connection, users: &[i32]) -> Result<bool> {
+        if self.kind() != ListType::User {
+            return Ok(false);
+        }
+        self.clear(conn)?;
+        self.add_users(conn, users)
+    }
+
+    pub fn set_blogs(&self, conn: &Connection, blogs: &[i32]) -> Result<bool> {
+        if self.kind() != ListType::Blog {
+            return Ok(false);
+        }
+        self.clear(conn)?;
+        self.add_blogs(conn, blogs)
+    }
+
+    pub fn set_words(&self, conn: &Connection, words: &[&str]) -> Result<bool> {
+        if self.kind() != ListType::Word {
+            return Ok(false);
+        }
+        self.clear(conn)?;
+        self.add_words(conn, words)
+    }
+
+    pub fn set_prefixes(&self, conn: &Connection, prefixes: &[&str]) -> Result<bool> {
+        if self.kind() != ListType::Prefix {
+            return Ok(false);
+        }
+        self.clear(conn)?;
+        self.add_prefixes(conn, prefixes)
     }
 }
 
@@ -169,7 +322,6 @@ pub(super) mod private {
 
     impl ListElem {
         insert!(list_elems, NewListElem);
-        list_by!(list_elems, for_list, list_id as i32);
 
         pub fn user_in_list(conn: &Connection, list: &List, user: i32) -> Result<bool> {
             dsl::select(dsl::exists(
