68dd8602b2d10327abf2464460ddfbe704a459bb
Baptiste Gelez <baptiste@gelez.xyz>
2019-04-19 13:59:03 +0100

    New Crowdin translations (#479)
    Enjoy!

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,9 @@ use std::process::exit;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-init_i18n!("plume", ar, de, en, es, fr, gl, it, ja, nb, pl, pt, ru);
+init_i18n!(
+    "plume", ar, bg, ca, cs, de, en, eo, es, fr, gl, hi, hr, it, ja, nb, pl, pt, ro, ru, sr, sk, sv
+);
 
 mod api;
 mod inbox;
