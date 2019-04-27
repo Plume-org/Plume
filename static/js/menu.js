da28849080d9e1ead56f87ac2d24ff8b46797e5c
Marek Ľach <mareklachbc@tutanota.com>
2019-04-27 12:02:38 +0200

    Restore menu.js file for iOS

---
+++ static/js/menu.js
@@ -0,0 +1,12 @@
+//focus-within is not yet supported by Webkit/Blink
+
+const button = document.getElementById('menu')
+const menu = document.getElementById('content')
+
+button.addEventListener('click', () => {
+  menu.classList.add('show')
+})
+
+menu.addEventListener('click', () => {
+  menu.classList.remove('show')
+})
