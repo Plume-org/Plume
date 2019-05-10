625aac938c01ea8b27101e2fd53ef22e36f64aff
Trinity Pointard <trinity.pointard@insa-rennes.fr>
2019-05-10 13:15:24 +0200

    upload artifact of wasm binary
    fix #570

--- script/generate_artifact.sh
+++ script/generate_artifact.sh
@@ -2,4 +2,5 @@
 mkdir bin
 cp target/release/{plume,plm} bin
 strip -s bin/*
-tar -cvzf plume.tar.gz bin/ static/ migrations/$FEATURES
+tar -cvzf plume.tar.gz bin/ static/
+tar -cvzf wasm.tar.gz static/plume-front.{js,wasm}
