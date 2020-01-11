e81cec76b26613dd9ad0af7cd85de4326e7f6fd7
Violet white <violet.white.dammit@protonmail.com>
2020-01-10 20:00:02 -0500

    Correct primary key datatype for postgresql

--- migrations/postgres/2020-01-05-232816_add_blocklist/up.sql
+++ migrations/postgres/2020-01-05-232816_add_blocklist/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
-create table email_blocklist(id integer primary key,
-                             email_address text,
+create table email_blocklist(id SERIAL primary key,
+                             email_address text ,
                              note text,
                              notify_user boolean default false,
                              notification_text text);
