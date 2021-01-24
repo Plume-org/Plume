aeab14ef067f0c51cecb46c1b8da73a73a7082be
Marek Ľach <graweeld@googlemail.com>
2021-01-24 16:41:00 +0000

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,10 @@
-
-
-
-
-    
-
 const button = document.getElementById('menu')
+const menu = document.getElementById('content')
 
-    
+button.addEventListener('click', () => {
+  menu.classList.add('show')
+})
+
+menu.addEventListener('click', () => {
+  menu.classList.remove('show')
+})
