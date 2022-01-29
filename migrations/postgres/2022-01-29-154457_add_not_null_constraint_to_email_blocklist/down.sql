858cad299554acd1ff0aa9990dff9091eb38b0d0
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2022-01-30 00:54:10 +0900

    Set null to email_blocklist table fields

--- migrations/postgres/2022-01-29-154457_add_not_null_constraint_to_email_blocklist/down.sql
+++ migrations/postgres/2022-01-29-154457_add_not_null_constraint_to_email_blocklist/down.sql
@@ -1,1 +1,4 @@
--- This file should undo anything in `up.sql`
+ALTER TABLE email_blocklist ALTER COLUMN notification_text DROP NOT NULL;
+ALTER TABLE email_blocklist ALTER COLUMN notify_user DROP NOT NULL;
+ALTER TABLE email_blocklist ALTER COLUMN note DROP NOT NULL;
+ALTER TABLE email_blocklist ALTER COLUMN email_address DROP NOT NULL;
--- migrations/postgres/2022-01-29-154457_add_not_null_constraint_to_email_blocklist/up.sql
+++ migrations/postgres/2022-01-29-154457_add_not_null_constraint_to_email_blocklist/up.sql
@@ -1,1 +1,4 @@
--- Your SQL goes here
+ALTER TABLE email_blocklist ALTER COLUMN email_address SET NOT NULL;
+ALTER TABLE email_blocklist ALTER COLUMN note SET NOT NULL;
+ALTER TABLE email_blocklist ALTER COLUMN notify_user SET NOT NULL;
+ALTER TABLE email_blocklist ALTER COLUMN notification_text SET NOT NULL;
