890c9a0da408bd2ba3f8fd5d203f2aa41268bb0c
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-01-09 16:19:05 +0900

    Add migration for SQLite

---
+++ migrations/sqlite/2023-01-09-070959_add_unique_constraint_to_activity_pub_related_fields/down.sql
@@ -0,0 +1,3 @@
+DROP INDEX users_fqn;
+DROP INDEX blogs_actor_id;
+DROP INDEX blogs_fqn;
---
+++ migrations/sqlite/2023-01-09-070959_add_unique_constraint_to_activity_pub_related_fields/up.sql
@@ -0,0 +1,3 @@
+CREATE UNIQUE INDEX users_fqn ON users (fqn);
+CREATE UNIQUE INDEX blogs_actor_id ON blogs (actor_id);
+CREATE UNIQUE INDEX blogs_fqn ON blogs (fqn);
